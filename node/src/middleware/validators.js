b80b4100b1db70ae51c59839702f499f3ee189d9
kf949s@gmail.com <kf949s@gmail.com>
2018-02-24 14:50:08 -0700

    Latest

--- node/src/middleware/validators.js
+++ node/src/middleware/validators.js
@@ -1,14 +1,19 @@
-import apiAuthenticationService from '../services/apiAuthenticationService'
+import apiAuthenticationService from '../services/apiAuthenticationService';
+import {config} from 'dotenv';
 
+const authService = new apiAuthenticationService();
+const api_protection = process.env.API_AUTHENTICATION
+config();
 
 export async function ensureUser (ctx, next) {
-  const authService = new apiAuthenticationService();
-
-  try {
-    const result = await apiAuthenticationService.authUser(ctx);
-    return next(ctx);
-  } catch (err) {
-    ctx.status = 401;
-    ctx.throw(err);
-  }
+    if(!api_protection) {
+      return next(ctx);
+    }
+    try {
+      const result = await apiAuthenticationService.authUser(ctx);
+      return next(ctx);
+    } catch (err) {
+      ctx.status = 401;
+      ctx.throw(err);
+    }
 }
